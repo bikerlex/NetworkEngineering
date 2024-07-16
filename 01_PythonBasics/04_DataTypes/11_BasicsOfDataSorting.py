d843996cc478353a2484f8ca6cc3fe40ba0d3b81
bikerlex <azagovorov.hd@gmail.com>
2024-07-16 21:51:45 +0700

    Основы сортировки данных

---
+++ 01_PythonBasics/04_DataTypes/11_BasicsOfDataSorting.py
@@ -0,0 +1,13 @@
+# Основы сортировки данных
+
+data = [[1, 100, 1000], [2, 2, 2], [1, 2, 3], [4, 100, 3]]
+print(data)
+print(sorted(data))
+
+vlans = ['1', '30', '11', '3', '10', '20', '30', '100']
+print(vlans)
+print(sorted(vlans))
+
+ip_list = ["10.1.1.1", "10.1.10.1", "10.1.2.1", "10.1.11.1"]
+print(ip_list)
+print(sorted(ip_list))
