d182c7293bc4437f5dc4d44d499b34d6ab5c117d
bikerlex <azagovorov.hd@gmail.com>
2024-07-19 00:29:54 +0700

    Ввод информации пользователем

---
+++ 01_PythonBasics/05_CreatingBasicScripts/03_UserInput.py
@@ -0,0 +1,19 @@
+# Ввод информации пользователем
+
+print(input('Твой любимый протокол маршрутизации? '))
+
+protocol = input('Твой любимый протокол маршрутизации? ')
+print(protocol)
+
+interface = input('Enter interface type and number: ')
+vlan = input('Enter VLAN number: ')
+
+access_template = ['switchport mode access',
+                   'switchport access vlan {}',
+                   'switchport nonegotiate',
+                   'spanning-tree portfast',
+                   'spanning-tree bpduguard enable']
+
+print('\n' + '-' * 30)
+print('interface {}'.format(interface))
+print('\n'.join(access_template).format(vlan))
